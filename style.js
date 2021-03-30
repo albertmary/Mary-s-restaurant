feda9b1aecee565d9a6845623fc9aee9c08bbf2d
albertmary <81420434+albertmary@users.noreply.github.com>
2021-03-30 19:53:39 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,14 @@
+
+
+function getStarted() {
+  
+  alert ('Hello Customer!');
+  
+  var CustomerInterest = confirm ('Would you like a taste of our classic dishes ');
+  
+  if (CustomerInterest == true) {
+    alert('We serve jollof rice at $1000 per plate');
+  } else {
+  alert ('Okay, let me get the menu for you');
+}
+}
